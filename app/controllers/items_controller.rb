b49e5a6b2219435754945916c650c60376bc9ac0
fyky <fuyu12dmb25ms@gmail.com>
2022-02-07 18:36:44 +0900

    [Add]JSONデータ取得、個別表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,29 @@
 class ItemsController < ApplicationController
+
+
   def index
+    # つけないと動かない　なぜ
+    require 'net/http'
+    # require 'uri'
+
+    # params = URI.encode_www_form({created: >2022-1-31})
+    params = "2022-1-31"
+    uri = URI.parse("https://qiita.com/api/v2/items?page=10&per_page=10&query=created:>#{params}")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    @title1 = result[0]["title"]
+    @created_at1 = result[0]["created_at"]
+    @lgtm1 = result[0]["likes_count"]
+
+    @title2 = result[1]["title"]
+    @created_at2 = result[1]["created_at"]
+    @lgtm2 = result[1]["likes_count"]
+
+    @title3 = result[2]["title"]
+    @created_at3 = result[2]["created_at"]
+    @lgtm3 = result[2]["likes_count"]
+    # :
+    # :
     
   end
 end
