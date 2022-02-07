66d9be65b8e87e393f0121e0aa32ea13d6a4b76a
fyky <fuyu12dmb25ms@gmail.com>
2022-02-07 20:10:34 +0900

    [Add]複数タグを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,15 +11,45 @@ class ItemsController < ApplicationController
     uri = URI.parse("https://qiita.com/api/v2/items?page=10&per_page=10&query=created:>#{params}")
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
-    @title1 = result[0]["title"]
-    @created_at1 = result[0]["created_at"]
-    @lgtm1 = result[0]["likes_count"]
+
+    # 1個目のみ
+    @title = result[0]["title"]
+    @name= result[0]["user"]["name"]
+    @created_at = result[0]["created_at"]
+    @lgtm = result[0]["likes_count"]
+    # 1個目のみ
+    @tag = result[0]["tags"]
+
+    @tag_array= []
+
+    # 配列で取得したい
+    @tag.each { |tag|
+      @tags = tag["name"]
+      @tag_array.append(@tags)
+    }
+    p @tag_array
+
+    # for i in @tag do
+    #   @tag
+    #   @tags.append
+    # end
+
+
+    # :
+    # :
+    
+    # for tag in result[0]["tags"] do
+    #   puts tag
+    # end 
+
 
     @title2 = result[1]["title"]
+    @name2= result[1]["user"]["name"]
     @created_at2 = result[1]["created_at"]
     @lgtm2 = result[1]["likes_count"]
 
     @title3 = result[2]["title"]
+    @name3= result[2]["user"]["name"]
     @created_at3 = result[2]["created_at"]
     @lgtm3 = result[2]["likes_count"]
     # :
