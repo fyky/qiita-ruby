595330bd26a2fcb30fe4f5d59c659648756a125a
fyky <fuyu12dmb25ms@gmail.com>
2022-02-09 20:15:20 +0900

    [Fix]変数名修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,83 +5,52 @@ class ItemsController < ApplicationController
 
     today = Date.today
     # 1週間前の日付を取得
-    params2 = (today - 7).strftime
+    params_date = (today - 7).strftime
 
-    # アクセストークン
+    # アクセストークン環境変数化
     access_token = ENV['access_token']
     headers = {'Authorization' => "Bearer #{access_token}"}
 
-    # pageを回して配列で取得
+    # pageを回して配列でデータを取得
     result = []
-    # # 1~3ページ目まで回す
     i = 1
-    while i <= 100 do
-    # for i in 1..100 do
-      uri = URI.parse("https://qiita.com/api/v2/items?page=#{i}&per_page=100&query=created:>#{params2}")
+    while i <= 2 do
+      uri = URI.parse("https://qiita.com/api/v2/items?page=#{i}&per_page=100&query=created:>#{params_date}")
       res = URI.open(uri, headers)
       result << JSON.parse(res.read)
       i += 1
     end
 
-    @items = []
+    # items[]に全てのデータを格納
+    items = []
     # binding.irb
     for f in 0..(result[0].size-1) do
       for val in result do
         if val[f]
-          @title = val[f]["title"]
-          @url = val[f]["url"]
-          @lgtm = val[f]["likes_count"]
-          @created_at = val[f]["created_at"]
-          @name = val[f]["user"]["name"]
-          @tag = val[f]["tags"]
-            @tag_names= []
-            # each文で回したいので配列で取得する
-            @tag.each { |t|
-              @tag_name = t["name"]
-              @tag_names.append(@tag_name)
+          title = val[f]["title"]
+          url = val[f]["url"]
+          lgtm = val[f]["likes_count"]
+          created_at = val[f]["created_at"]
+          name = val[f]["user"]["name"]
+          tag = val[f]["tags"]
+            tag_names= []
+            # each文で回して取得する
+            tag.each { |t|
+              tag_name = t["name"]
+              tag_names.append(tag_name)
             }
-          @items.append(@title,@url,@lgtm,@created_at,@name,@tag_names)
+          items.append(title,url,lgtm,created_at,name,tag_names)
         end
-          # p @tag_names
       end
     end
-      @all_items = @items.each_slice(6).to_a
-      # @all_itemss = @items.each_slice(6).to_a.sort[2]
-      # p @all_items
-      @all_items.sort_by! { |a| a[2] }
-      @liked_all_items = @all_items.reverse
-      # @liked_all_items = Kaminari.paginate_array(@liked_all_items).page(1).per(2)
+      # itemsから1セット（6つのデータ）ずつ切り分けて格納
+      set_items = items.each_slice(6).to_a
+      # 要素3つ目(lgtm数で並び替え)
+      set_items.sort_by! { |a| a[2] }
+      # lgtm数の多い順に表示
+      liked_all_items = set_items.reverse
 
-      # order("liked_at": :DESC)
-      # @all_itemsss = @items.each_slice(6)
-      # p @all_itemsss
-      # binding.irb
-      # p @liked_all_items[0]
-
-      @array2 = []
-      @liked_all_items.each do |f|
-        @array2.push({
-          title: f[0],
-          url: f[1],
-          lgtm: f[2],
-          created_at: f[3],
-          name: f[4],
-          tag: f[5]
-        })
-      end
-      # @array3 = @array2.each_slice(6).to_a
-      # p @array2.each_slice(6).to_a
-
-      # @allitemss = @array2.page(params[:page]).per(1)
-      # for N in 0..3 do
-
-      # p @items.each_slice(6)
-  # binding.irb
-        @all_itemss = Kaminari.paginate_array(@liked_all_items).page(params[:page]).per(20)
-        # p @all_itemss
-      # end
-
-      # p @items
-
+      # ページネーション
+      @all_items = Kaminari.paginate_array(liked_all_items).page(params[:page]).per(20)
   end
 end
