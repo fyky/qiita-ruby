f7507dbf704286fec77e820ad1e3b626c8767371
fyky <fuyu12dmb25ms@gmail.com>
2022-02-09 12:57:23 +0900

    [Add]トークン

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,96 +1,66 @@
 class ItemsController < ApplicationController
   def index
-    # つけないと動かない　なぜ
     require 'net/http'
-    # require 'uri'
+    require 'open-uri'
 
-    # params = URI.encode_www_form({created: >2022-1-31})
-    # 日付を仮定
     today = Date.today
+    # 1週間前の日付を取得
     params = (today - 7).strftime
-    # params = "2022-1-31"
-    # uri = URI.parse("https://qiita.com/api/v2/items?page=1&per_page=3&query=created:>#{params}")
-    # response = Net::HTTP.get_response(uri)
-    # result = JSON.parse(response.body)
-    # p result
 
+    # アクセストークン
+    access_token = ENV['access_token']
+    headers = {'Authorization' => "Bearer #{access_token}"}
 
+    # pageを回して配列で取得
     result = []
-    for i in 1..3 do
+    # # 1~3ページ目まで回す
+    i = 1
+    while i <= 2 do
+    # for i in 1..100 do
       uri = URI.parse("https://qiita.com/api/v2/items?page=#{i}&per_page=2&query=created:>#{params}")
-      response = Net::HTTP.get_response(uri)
-      result << JSON.parse(response.body)
+      res = URI.open(uri, headers)
+      result << JSON.parse(res.read)
+      i += 1
+      # response = Net::HTTP.get_response(uri, headers)
+      # result << JSON.parse(response.body)
+      # i += 1
+      # result << JSON.parse(uri.read)
     end
-    p result.size
+
+    # binding.irb
+    # p result.size
+    # result = [[{"title"=>"タイトル"},{"lgtm"=>"10"},{"user"=>{"name"=>"uu"}},{"created_at"=>"x"},{"tag"=>"y"}],
+    # [{"title"=>"タイトル"},{"lgtm"=>"10"},{"user"=>{"name"=>"uu"}},{"created_at"=>"x"},{"tag"=>"y"}],
+    # [{"title"=>"タイトル"},{"lgtm"=>"10"},{"user"=>{"name"=>"uu"}},{"created_at"=>"x"},{"tag"=>"y"}]]
+    # p result[0]["user"]["name"]
 
     # ページ数＝配列＝size
 
-    # resultは配列　itemがたくさん入っている　
-    # 配列がなくなるまで繰り返し表示させる必要がある
-    # itemにはtitle,name,created_at,lgtm,tagsのセットが1つ入っている
-    @test = result
-    # puts @test
-
     @items = []
     # binding.irb
-    # for i in 0..@test.size do
-    # end
-
-    # for f in 0..2 do
-    # これで動く？
-    # end
-    for f in 0..(@test[0].size-1) do
-      for val in @test do
-        @title = val[f]["title"]
-        @lgtm = val[f]["likes_count"]
-        @created_at = val[f]["created_at"]
-        @name = val[f]["user"]["name"]
-        @tag = val[f]["tags"]
-          @tag_names= []
-          # each文で回したいので配列で取得する
-          @tag.each { |t|
-            @tag_name = t["name"]
-            @tag_names.append(@tag_name)
-          }
-        @items.append(@title,@lgtm,@created_at,@name,@tag_names)
+    for f in 0..(result[0].size-1) do
+      for val in result do
+        if val[f]
+          @title = val[f]["title"]
+          @url = val[f]["url"]
+          @lgtm = val[f]["likes_count"]
+          @created_at = val[f]["created_at"]
+          @name = val[f]["user"]["name"]
+          @tag = val[f]["tags"]
+            @tag_names= []
+            # each文で回したいので配列で取得する
+            @tag.each { |t|
+              @tag_name = t["name"]
+              @tag_names.append(@tag_name)
+            }
+          @items.append(@title,@url,@lgtm,@created_at,@name,@tag_names)
+        end
           # p @tag_names
-
-        # # no implicit でエラーが出たので修正必要 数値がくるべきところに文字がきた
-        # @title = val[f]["title"]
-        # @lgtm = "#{val[f]["likes_count"]}"
-        # @created_at =  "#{val[f]["created_at"]}"
-        # @name =  "#{val[f]["user"]["name"]}"
-        # @tag = val[f]["tags"]
-        #   # p @tag
-        #   @tag_names= []
-        #   # each文で回したいので配列で取得する
-        #   @tag.each { |t|
-        #     @tag_name = t["name"]
-        #     @tag_names.append(@tag_name)
-        #   }
-        #   # p @tag_names
-        # # p "tagは" + @tag
-        # @items.append(@title,@lgtm,@created_at,@name,@tag_names)
       end
     end
-      # p "配列の中身は" + @items.to_s
-      @all_items = @items.each_slice(5).to_a
+      @all_items = @items.each_slice(6).to_a
+      # .page(params[:page]).per(3)
       p @all_items
 
-    # # 1個目のみ
-    # @title = result[0]["title"]
-    # @name= result[0]["user"]["name"]
-    # @created_at = result[0]["created_at"]
-    # @lgtm = result[0]["likes_count"]
-    # 1個目のみ
-    # @tags = result[0]["tags"]
-    #   p @tags
-    # @tag_names= []
-    # # each文で回したいので配列で取得する
-    # @tags.each { |tag|
-    #   @tag_name = tag["name"]
-    #   @tag_names.append(@tag_name)
-    # }
-    # p @tag_names
   end
 end
